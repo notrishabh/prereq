5c96d6baca21ad63e59d8119975a5fa1d895b1e9
Rishabh <dplayarrr@gmail.com>
2025-01-21 01:04:06 +0530

    ADD: Added groq library and /ai route api

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/notrishabh/prereq
 
 go 1.23.4
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
+	github.com/jpoz/groq v0.0.0-20240513145022-7a02894105a0
+)
--- main.go
+++ main.go
@@ -1,27 +1,53 @@
 package main
 
 import (
-	"bytes"
-	"context"
 	"encoding/json"
-	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
-	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"github.com/jpoz/groq"
 )
 
-type Msg struct {
-	Role    string `json:"role"`
-	Content string `json:"content"`
+func getAIresponse(prompt string) (string, error) {
+	apiKey := os.Getenv("GROQ_API_KEY")
+
+	client := groq.NewClient(groq.WithAPIKey(apiKey))
+
+	chatCompletion, err := client.CreateChatCompletion(groq.CompletionCreateParams{
+		Model: "llama3-8b-8192",
+		Messages: []groq.Message{
+			{
+				Role:    "system",
+				Content: "You are UI/UX designer. Your primary role is to assist the user in settings the prerequisites like the perfect color scheme, font families, hero images,etc. for designing an application or a modern website according to the prompt of the user",
+			},
+			{
+				Role:    "user",
+				Content: prompt,
+			},
+		},
+		// Stream: true,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	// for delta := range chatCompletion.Stream {
+	// 	fmt.Print(delta.Choices[0].Delta.Content)
+	// }
+	return chatCompletion.Choices[0].Message.Content, nil
 }
 
-type Body struct {
-	Model    string `json:"model"`
-	Messages []Msg  `json:"messages"`
+func aiResponseHandler(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query().Get("q")
+	resp, err := getAIresponse(q)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(resp)
 }
 
 func main() {
@@ -29,57 +55,9 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
-	apiKey := "Bearer " + os.Getenv("APIKEY")
-	baseURL := "https://api.groq.com/openai/v1/chat/completions"
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-
-	x := Body{
-		Model: "llama-3.3-70b-versatile",
-		Messages: []Msg{
-			{
-				Role:    "system",
-				Content: "You are UI/UX designer. Your primary role is to assist the user in settings the prerequisites like the perfect color scheme, font families, hero images,etc. for designing an application or a modern website according to the prompt of the user",
-			},
-			{
-				Role:    "user",
-				Content: "I want to create an ecommerce website to sell fashion products such as clothes and accessories.",
-			},
-		},
-	}
-
-	jsonBody, err := json.Marshal(x)
-	if err != nil {
-		fmt.Printf("Error parsing JSON: %v", err)
-		return
-	}
-
-	req, err := http.NewRequestWithContext(ctx, "POST", baseURL, bytes.NewBuffer(jsonBody))
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", apiKey)
-
-	client := http.DefaultClient
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Printf("Error in sending req: %v", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	res, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("Error while reading resp: %v", err)
-		return
-	}
-	var d ChatCompletion
-	err = json.Unmarshal(res, &d)
-	if err != nil {
-		fmt.Printf("Error while marshalling resp: %v", err)
-		return
-	}
-	fmt.Println(d.Choices[0].Message.Content)
-	return
+	port := "8080"
+	r := mux.NewRouter()
+	r.HandleFunc("/ai", aiResponseHandler).Methods("GET")
+	log.Printf("server starting on port: %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }

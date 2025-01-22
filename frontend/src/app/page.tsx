999cc3780278e1db3f11a6151d0d74d37178a212
Rishabh <dplayarrr@gmail.com>
2025-01-23 01:12:26 +0530

    ADD: UI styles

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -18,15 +18,22 @@ export default function Home() {
       method: "post",
       body: JSON.stringify(oldQues),
     });
+    const text = await x.text();
+    const res = text
+      .slice(1, -1)
+      .replaceAll("\\n", "\n")
+      .replaceAll("\\t", "\t");
     setOldQues([
       ...oldQues,
       {
         Role: "user",
         Content: ques,
       },
+      {
+        Role: "system",
+        Content: res,
+      },
     ]);
-    const text = await x.text();
-    const res = text.slice(1, -1).replaceAll("\\n", "\n");
     setResponse(res);
   };
 
@@ -37,29 +44,57 @@ export default function Home() {
   const handleSubmitForm = (event: React.FormEvent) => {
     event.preventDefault();
     getAiResponse();
+    setQues("");
+  };
+
+  const chatScreen = () => {
+    return (
+      <div className="mt-10 mb-8 h-[calc(100vh-22rem)] w-2/3 overflow-auto">
+        {oldQues.map((q: groqmsg, i) => (
+          <div
+            key={i}
+            className={`flex ${q.Role === "user" ? "justify-end" : ""}`}
+          >
+            <Markdown
+              key={i}
+              className={`text-lg mb-8 whitespace-pre-line ${q.Role === "user" ? "bg-gray-700 px-4 py-2 rounded-2xl" : ""}`}
+            >
+              {q.Content}
+            </Markdown>
+          </div>
+        ))}
+      </div>
+    );
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <section className="space-y-8 flex flex-col justify-center items-center w-full">
-        <h1 className="text-3xl">What are you building?</h1>
-        <form onSubmit={handleSubmitForm}>
-          <input
-            onChange={handleInput}
-            className="p-4 bg-gray-800 text-xl w-2/3 rounded-2xl"
-          />
-          <button
-            type="submit"
-            className="rounded-xl border-2 border-white p-2"
-          >
-            Generate
-          </button>
-        </form>
-        {response && (
-          <div className="space-y-2 p-4 border-2 border-white rounded-xl">
-            <Markdown>{response}</Markdown>
-          </div>
+    <main>
+      <div className="fixed text-3xl p-8">First Step</div>
+      <section className="min-h-screen flex flex-col justify-center items-center w-full">
+        {response ? (
+          chatScreen()
+        ) : (
+          <h1 className="text-3xl mb-8">What are you building?</h1>
         )}
+        <form
+          onSubmit={handleSubmitForm}
+          className="flex w-full justify-center items-center"
+        >
+          <div className="relative w-2/3">
+            <input
+              onChange={handleInput}
+              placeholder="Ask anything"
+              value={ques}
+              className="p-4 bg-gray-800 text-xl w-full rounded-2xl pr-20"
+            />
+            <button
+              type="submit"
+              className=" text-4xl text-gray-400 bg-gray-700 rounded-full px-4 pt-1 absolute top-0 right-0 bottom-0 m-1 mr-3"
+            >
+              &#8593;
+            </button>
+          </div>
+        </form>
       </section>
     </main>
   );

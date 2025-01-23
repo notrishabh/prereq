6d727d2f9d7901b2d78f6fb3c2177053b3206cc9
Rishabh <dplayarrr@gmail.com>
2025-01-24 03:43:40 +0530

    ADD: Show hex to colors in chat

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,9 +8,14 @@ type groqmsg = {
   Content: string;
 };
 
+type hexRes = {
+  chat: groqmsg;
+  hexArr?: string[];
+};
+
 export default function Home() {
   const [ques, setQues] = useState("");
-  const [chat, setChat] = useState<groqmsg[]>([]);
+  const [chatWithHex, setChatWithHex] = useState<hexRes[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const lastMsgRef = React.createRef<HTMLDivElement>();
 
@@ -18,25 +23,51 @@ export default function Home() {
     lastMsgRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [lastMsgRef]);
 
+  const extractHex = (content: string): string[] => {
+    let idx = 0;
+    const hexArr: string[] = [];
+    while (idx < content.length) {
+      idx = content.indexOf("#", idx);
+      const hex = content.slice(idx, idx + 7);
+      if (idx != -1) {
+        // Validate if it's a valid hex code
+        if (/^#[0-9a-fA-F]{6}$/.test(hex)) {
+          if (!hexArr.includes(hex)) {
+            hexArr.push(hex);
+          }
+        }
+      } else {
+        break;
+      }
+      idx += 7;
+    }
+    return hexArr;
+  };
+
   const getAiResponse = async () => {
     const x = await fetch(`http://localhost:8080/ai?q=${ques}`, {
       method: "post",
-      body: JSON.stringify(chat),
+      body: JSON.stringify(chatWithHex.map((r) => r.chat)),
     });
     const text = await x.text();
     const res = text
       .slice(1, -1)
       .replaceAll("\\n", "\n")
       .replaceAll("\\t", "\t");
-    setChat([
-      ...chat,
+    setChatWithHex([
+      ...chatWithHex,
       {
-        Role: "user",
-        Content: ques,
+        chat: {
+          Role: "user",
+          Content: ques,
+        },
       },
       {
-        Role: "system",
-        Content: res,
+        chat: {
+          Role: "system",
+          Content: res,
+        },
+        hexArr: extractHex(res),
       },
     ]);
     setLoading(false);
@@ -62,16 +93,29 @@ export default function Home() {
   const chatScreen = () => {
     return (
       <div className="mt-10 mb-8 h-[calc(100vh-22rem)] w-2/3 overflow-auto">
-        {chat.map((q: groqmsg, i) => (
+        {chatWithHex.map((q: hexRes, i) => (
           <div
             key={i}
-            className={`flex ${q.Role === "user" ? "justify-end" : ""}`}
+            className={`flex ${q.chat.Role === "user" ? "justify-end" : ""}`}
           >
             <Markdown
-              className={`text-lg mb-8 whitespace-pre-line ${q.Role === "user" ? "bg-gray-700 px-4 py-2 rounded-2xl" : ""}`}
+              className={`text-lg mb-8 whitespace-pre-line ${q.chat.Role === "user" ? "bg-gray-700 px-4 py-2 rounded-2xl" : "flex-1"}`}
             >
-              {q.Content}
+              {q.chat.Content}
             </Markdown>
+            {q.hexArr && q.hexArr.length ? (
+              <div className="border border-gray-700 rounded-xl h-max p-4 min-w-48 space-y-2">
+                {q.hexArr.map((hex, i) => (
+                  <p key={i} className="flex gap-2">
+                    <span>{hex}</span>
+                    <span>&#8594;</span>
+                    <div style={{ background: hex }} className="w-20 h-5"></div>
+                  </p>
+                ))}
+              </div>
+            ) : (
+              ""
+            )}
           </div>
         ))}
         {loading && <Loading />}
@@ -84,7 +128,7 @@ export default function Home() {
     <main>
       <div className="fixed text-3xl p-8">First Step</div>
       <section className="min-h-screen flex flex-col justify-center items-center w-full">
-        {chat.length ? (
+        {chatWithHex.length ? (
           chatScreen()
         ) : (
           <h1 className="text-3xl mb-8">What are you building?</h1>
@@ -96,7 +140,7 @@ export default function Home() {
           <div className="relative w-2/3">
             <input
               onChange={handleInput}
-              placeholder="Ask anything"
+              placeholder="Enter your million dollar idea"
               value={ques}
               className="p-4 bg-gray-800 text-xl w-full rounded-2xl pr-20"
             />
